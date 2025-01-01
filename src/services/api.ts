737b8c6bcd42c1aa17ffa868a644c66c4d8978a7
Chung Phong <phonghoang0205@gmail.com>
2025-01-01 22:35:25 +0700

    #70. Hiển Thị HomePage

--- src/services/api.ts
+++ src/services/api.ts
@@ -90,7 +90,11 @@ export const deleteUserAPI = (_id: string) => {
 
 export const getBooksAPI = (query: string) => {
   const urlBackend = `/api/v1/book?${query}`;
-  return axios.get<IBackendRes<IModelPaginate<IBookTable>>>(urlBackend);
+  return axios.get<IBackendRes<IModelPaginate<IBookTable>>>(urlBackend, {
+    headers: {
+      delay: 250,
+    },
+  });
 };
 
 export const getCategoryAPI = () => {
