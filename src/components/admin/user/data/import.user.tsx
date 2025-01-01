f712007ea4ef844bb656873b3d005d9064f22447
Chung Phong <phonghoang0205@gmail.com>
2025-01-01 10:47:45 +0700

    #55. Download Sample File

--- src/components/admin/user/data/import.user.tsx
+++ src/components/admin/user/data/import.user.tsx
@@ -6,7 +6,7 @@ import { useState } from "react";
 import Exceljs from "exceljs";
 import { Buffer } from "buffer";
 import { bulkCreateUserAPI } from "@/services/api";
-
+import templateFile from "assets/template/user.xlsx?url";
 const { Dragger } = Upload;
 interface IProps {
   openModalImport: boolean;
@@ -129,7 +129,15 @@ const ImportUser = (props: IProps) => {
             Click or drag file to this area to upload
           </p>
           <p className="ant-upload-hint">
-            Support for a single upload. Only accept .csv, .xls, .xlsx
+          Support for a single upload. Only accept .csv, .xls, .xlsx . or
+                        &nbsp;
+                        <a
+                            onClick={e => e.stopPropagation()}
+                            href={templateFile}
+                            download
+                        >
+                            Download Sample File
+                        </a>
           </p>
         </Dragger>
         <div style={{ paddingTop: 20 }}>
--- src/components/admin/user/table.user.tsx
+++ src/components/admin/user/table.user.tsx
@@ -122,12 +122,12 @@ const TableUser = () => {
             }
           }
           //default
-          query += `&sort=-createdAt`;
+
           if (sort && sort.createdAt) {
             query += `&sort=${
               sort.createdAt === "ascend" ? "createdAt" : "-createdAt"
             }`;
-          }
+          } else query += `&sort=-createdAt`;
           const res = await getUsersAPI(query);
           if (res.data) {
             setMeta(res.data.meta);
