71e0cef41a437f3618998e566d620c6a76ae2fe3
Chung Phong <phonghoang0205@gmail.com>
2025-01-01 20:13:19 +0700

    #66.Create a New Book

--- src/components/admin/book/table.book.tsx
+++ src/components/admin/book/table.book.tsx
@@ -238,6 +238,7 @@ const TableBook = () => {
       <CreateBook
         openModalCreate={openModalCreate}
         setOpenModalCreate={setOpenModalCreate}
+        refreshTable={refreshTable}
       />
     </>
   );
