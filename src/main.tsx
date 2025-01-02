8ffe740e1841e9baede78146cb81defe77cd15a2
Chung Phong <phonghoang0205@gmail.com>
2025-01-02 11:27:17 +0700

    #81. Order Page

--- src/main.tsx
+++ src/main.tsx
@@ -16,6 +16,7 @@ import ManageBookPage from "pages/admin/manage.book";
 import ManageOrderPage from "pages/admin/manage.order";
 import ManageUserPage from "pages/admin/manage.user";
 import LayoutAdmin from "components/layout/layout.admin";
+import OrderPage from "pages/client/order";
 import enUS from "antd/locale/en_US";
 import viVN from "antd/locale/vi_VN";
 const router = createBrowserRouter([
@@ -30,6 +31,10 @@ const router = createBrowserRouter([
       {
         path: "/book/:id",
         element: <BookPage />,
+      },
+      {
+        path: "/order",
+        element: <OrderPage />,
       },
       {
         path: "/about",
---
+++ src/pages/client/order.tsx
@@ -0,0 +1,9 @@
+import OrderDetail from "@/components/client/order";
+const OrderPage = () => {
+  return (
+    <>
+      <OrderDetail />
+    </>
+  );
+};
+export default OrderPage;
